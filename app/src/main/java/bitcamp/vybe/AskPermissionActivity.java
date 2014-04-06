e1de6b942ca4d09d7436b1b49cb35fa3d4474ebe
hrosenblume <hrosenblume@gmail.com>
2014-04-05 23:27:06 -0400

    initial screen done

--- app/src/main/java/bitcamp/vybe/AskPermissionActivity.java
+++ app/src/main/java/bitcamp/vybe/AskPermissionActivity.java
@@ -1,18 +1,18 @@
 package bitcamp.vybe;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class AskPermission extends ActionBarActivity {
+public class AskPermissionActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ask_permission);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
